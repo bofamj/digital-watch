ea29c21b6bc614d4e27128b638c067ff42b51bcf
mustafa <mustafamjaber@yahoo.com>
2021-04-21 18:51:59 +0200

    coding the watch
    creat digital watch

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+function digitalCock(){
+
+let newDayeTime = new Date ();
+let corentHouers = newDayeTime.getHours();
+let corentMinutes = newDayeTime.getMinutes();
+let corentSeconds = newDayeTime.getSeconds();
+
+const houers = document.getElementById ('houres');
+const minutes = document.getElementById ('munets');
+const seconds = document.getElementById ('second');
+
+
+houers.innerHTML = corentHouers ;
+minutes .innerHTML =  corentMinutes;
+seconds .innerHTML =  corentSeconds;
+
+}
+
+setInterval(digitalCock , 1000);
