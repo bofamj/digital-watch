da27acd7808727897aa62ba380d5d6e07aa27ed3
mustafa <mustafamjaber@yahoo.com>
2021-04-21 19:03:14 +0200

    Time adjustment increment zero
    adjustment

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ let corentSeconds = newDayeTime.getSeconds();
 const houers = document.getElementById ('houres');
 const minutes = document.getElementById ('munets');
 const seconds = document.getElementById ('second');
-
+if ( corentHouers < 10){
+    corentHouers = "0 "+ corentHouers;
+}if ( corentMinutes < 10){
+    corentMinutes = "0 " + corentMinutes;
+}if ( corentSeconds < 10){
+    corentSeconds = "0 " + corentSeconds;
+}
 
 houers.innerHTML = corentHouers ;
 minutes .innerHTML =  corentMinutes;
